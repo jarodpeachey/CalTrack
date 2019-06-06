e3283b99ff8aecfa90c14295e6d10e3a8fc187b4
Jarod Peachey <jwpeachey107@aol.com>
2019-06-06 17:05:25 -0400

    Got router working and set up to display properly

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { render } from 'react-dom';
-import { Router } from 'react-router-dom';
+import {
+  browserHistory, Router, Route,
+} from 'react-router';
+// import { useScroll } from 'react-router-scroll';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { ThemeProvider } from 'styled-components';
-import routes from './components/Routes';
 import muiTheme from './mui-theme';
 import styledTheme from './styled-theme';
+import Main from './components/pages/Main';
+import Login from './components/pages/Login';
+import Signup from './components/pages/Signup';
+import Dashboard from './components/pages/Dashboard';
+import Meals from './components/pages/Meals';
+import Workouts from './components/pages/Workouts';
 
 function startApp () {
   render(
     <MuiThemeProvider theme={muiTheme}>
       <ThemeProvider theme={styledTheme}>
-        <Router>
-          {routes()}
+        <Router
+          history={browserHistory}
+        >
+          <Route path="/" component={Main} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/meals" component={Meals} />
+          <Route path="/workouts" component={Workouts} />
         </Router>
       </ThemeProvider>
     </MuiThemeProvider>, document.getElementById('app'),
