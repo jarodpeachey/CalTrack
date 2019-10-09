a699bfc5771e94416c8dd9a2d4e84aab0db67434
Jarod Peachey <jwpeachey107@aol.com>
2019-10-09 11:58:50 -0400

    Add no access page to dashboard

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -41,114 +41,47 @@ class Dashboard extends Component {
     }
   }
 
+  pushToPage (path) {
+    this.props.history.push(`${path}`);
+  }
+
   render () {
     const { classes, currentUser } = this.props;
     const { meals, workouts, calories } = this.state;
 
     return (
-      <Wrapper>
-        <div className="container py-sm">
-          {!meals.length && !workouts.length ? (
-            <Card className="card border no-shadow px-sm py-sm mb-sm">
-              <h4 className="m-none mb-xs">
-                Welcome,
-                {` ${currentUser.name}!`}
-              </h4>
-              <p className="m-none">
-                Get started with CalTrack by adding a meal or a workout!
-              </p>
-              <div className="row mobile">
-                <div className="col col-6">
-                  <Link to="/meals">
-                    <Button
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      className="m-none"
-                    >
-                      Add Meal
-                    </Button>
-                  </Link>
-                </div>
-                <div className="col col-6">
-                  <Link to="/workouts">
-                    <Button
-                      fullWidth
-                      variant="contained"
-                      color="primary"
-                      className="m-none"
-                    >
-                      Add Workout
-                    </Button>
-                  </Link>
-                </div>
-              </div>
-            </Card>
-          ) : (
-            <div className="row">
-              {calories && (
-                <div className="col col-12">
-                  <Card className="card border px-sm pt-lg pb-xxs mb-sm no-shadow position-relative">
-                    <Title fullWidth className="title mb-none">
-                      Calories
-                    </Title>
-                    <div className="mt-sm row mobile-lg collection">
-                      <div className="col col-4">
-                        <CollectionItem>
-                          <CalorieNumber>{calories.gained}</CalorieNumber>
-                          <CalorieTitle>Calories Gained</CalorieTitle>
-                        </CollectionItem>
-                      </div>
-                      <div className="col col-4">
-                        <CollectionItem>
-                          <CalorieNumber>{calories.lost}</CalorieNumber>
-                          <CalorieTitle>Calories Lost</CalorieTitle>
-                        </CollectionItem>
-                      </div>
-                      <div className="col col-4">
-                        <CollectionItem>
-                          <CalorieNumber>{calories.net}</CalorieNumber>
-                          <CalorieTitle>Net Calories</CalorieTitle>
-                        </CollectionItem>
-                      </div>
-                    </div>
-                  </Card>
-                </div>
-              )}
-              <div className="col col-6 py-none">
-                <Card className="card border px-sm pt-lg pb-md mb-sm no-shadow position-relative">
-                  <Title fullWidth className="title mb-none">
-                    Meals
-                  </Title>
-                  {meals.length ? (
-                    <>
-                      <ul className="collection my-md">
-                        {meals.map((meal, index) => {
-                          if (index <= 2) {
-                            return (
-                              <MealItemCompressed
-                                key={`mealItem-${meal.id}`}
-                                meal={meal}
-                              />
-                            );
-                          }
-                        })}
-                      </ul>
+      <>
+        {Object.keys(currentUser).length === 0 && currentUser.constructor === Object ? (
+          <div className="container">
+            <div className="center-text">
+              <Card className="card border no-shadow px-sm py-sm mb-sm">
+                <h3>You must be logged in to access this page.</h3>
+                <Button variant="contained" color="primary" onClick={() => this.pushToPage('/login')}>
+                  Go To Login
+                </Button>
+                <Button variant="contained" color="primary" onClick={() => this.pushToPage('/signup')}>
+                  Go To Signup
+                </Button>
+              </Card>
+            </div>
+          </div>
+        ) : (
+          <Wrapper>
+            <div className="container py-sm">
+              {!meals.length && !workouts.length ? (
+                <Card className="card border no-shadow px-sm py-sm mb-sm">
+                  <h4 className="m-none mb-xs">
+                    Welcome,
+                    {` ${currentUser.name}!`}
+                  </h4>
+                  <p className="m-none">
+                    Get started with CalTrack by adding a meal or a workout!
+                  </p>
+                  <div className="row mobile">
+                    <div className="col col-6">
                       <Link to="/meals">
                         <Button
-                          classes={{ root: classes.button }}
-                          color="primary"
-                          className="m-none"
-                        >
-                          See More Meals
-                        </Button>
-                      </Link>
-                    </>
-                  ) : (
-                    <div className="center-text">
-                      <h4 className="mt-sm">There are no meals! Eat up!</h4>
-                      <Link to="/meals">
-                        <Button
+                          fullWidth
                           variant="contained"
                           color="primary"
                           className="m-none"
@@ -157,45 +90,10 @@ class Dashboard extends Component {
                         </Button>
                       </Link>
                     </div>
-                  )}
-                </Card>
-              </div>
-              <div className="col col-6 py-none">
-                <Card className="card border px-sm pt-lg pb-md mb-sm no-shadow position-relative">
-                  <Title fullWidth className="title mb-none">
-                    Workouts
-                  </Title>
-                  {workouts.length ? (
-                    <>
-                      <ul className="collection my-md">
-                        {workouts.map((workout, index) => {
-                          if (index <= 2) {
-                            return (
-                              <WorkoutItemCompressed
-                                key={`workoutItem-${workout.id}`}
-                                workout={workout}
-                              />
-                            );
-                          }
-                        })}
-                      </ul>
+                    <div className="col col-6">
                       <Link to="/workouts">
                         <Button
-                          classes={{ root: classes.button }}
-                          color="primary"
-                          className="m-none"
-                        >
-                          See More Workouts
-                        </Button>
-                      </Link>
-                    </>
-                  ) : (
-                    <div className="center-text">
-                      <h4 className="mt-sm">
-                        There are no workouts! Get cracking!!
-                      </h4>
-                      <Link to="/workouts">
-                        <Button
+                          fullWidth
                           variant="contained"
                           color="primary"
                           className="m-none"
@@ -204,13 +102,138 @@ class Dashboard extends Component {
                         </Button>
                       </Link>
                     </div>
-                  )}
+                  </div>
                 </Card>
-              </div>
+              ) : (
+                <div className="row">
+                  {calories && (
+                    <div className="col col-12">
+                      <Card className="card border px-sm pt-lg pb-xxs mb-sm no-shadow position-relative">
+                        <Title fullWidth className="title mb-none">
+                          Calories
+                        </Title>
+                        <div className="mt-sm row mobile-lg collection">
+                          <div className="col col-4">
+                            <CollectionItem>
+                              <CalorieNumber>{calories.gained}</CalorieNumber>
+                              <CalorieTitle>Calories Gained</CalorieTitle>
+                            </CollectionItem>
+                          </div>
+                          <div className="col col-4">
+                            <CollectionItem>
+                              <CalorieNumber>{calories.lost}</CalorieNumber>
+                              <CalorieTitle>Calories Lost</CalorieTitle>
+                            </CollectionItem>
+                          </div>
+                          <div className="col col-4">
+                            <CollectionItem>
+                              <CalorieNumber>{calories.net}</CalorieNumber>
+                              <CalorieTitle>Net Calories</CalorieTitle>
+                            </CollectionItem>
+                          </div>
+                        </div>
+                      </Card>
+                    </div>
+                  )}
+                  <div className="col col-6 py-none">
+                    <Card className="card border px-sm pt-lg pb-md mb-sm no-shadow position-relative">
+                      <Title fullWidth className="title mb-none">
+                        Meals
+                      </Title>
+                      {meals.length ? (
+                        <>
+                          <ul className="collection my-md">
+                            {meals.map((meal, index) => {
+                              if (index <= 2) {
+                                return (
+                                  <MealItemCompressed
+                                    key={`mealItem-${meal.id}`}
+                                    meal={meal}
+                                  />
+                                );
+                              }
+                            })}
+                          </ul>
+                          <Link to="/meals">
+                            <Button
+                              classes={{ root: classes.button }}
+                              color="primary"
+                              className="m-none"
+                            >
+                              See More Meals
+                            </Button>
+                          </Link>
+                        </>
+                      ) : (
+                        <div className="center-text">
+                          <h4 className="mt-sm">There are no meals! Eat up!</h4>
+                          <Link to="/meals">
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              className="m-none"
+                            >
+                              Add Meal
+                            </Button>
+                          </Link>
+                        </div>
+                      )}
+                    </Card>
+                  </div>
+                  <div className="col col-6 py-none">
+                    <Card className="card border px-sm pt-lg pb-md mb-sm no-shadow position-relative">
+                      <Title fullWidth className="title mb-none">
+                        Workouts
+                      </Title>
+                      {workouts.length ? (
+                        <>
+                          <ul className="collection my-md">
+                            {workouts.map((workout, index) => {
+                              if (index <= 2) {
+                                return (
+                                  <WorkoutItemCompressed
+                                    key={`workoutItem-${workout.id}`}
+                                    workout={workout}
+                                  />
+                                );
+                              }
+                            })}
+                          </ul>
+                          <Link to="/workouts">
+                            <Button
+                              classes={{ root: classes.button }}
+                              color="primary"
+                              className="m-none"
+                            >
+                              See More Workouts
+                            </Button>
+                          </Link>
+                        </>
+                      ) : (
+                        <div className="center-text">
+                          <h4 className="mt-sm">
+                            There are no workouts! Get cracking!!
+                          </h4>
+                          <Link to="/workouts">
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              className="m-none"
+                            >
+                              Add Workout
+                            </Button>
+                          </Link>
+                        </div>
+                      )}
+                    </Card>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </Wrapper>
+          </Wrapper>
+        )}
+      </>
+
     );
   }
 }
